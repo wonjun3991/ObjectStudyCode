cb1e852610520e7a00a8acac998e5c4b9cb0e2e6
wonjun <wonjun.choi@meshkorea.net>
2022-06-04 18:24:39 +0900

    리팩토링 2

--- src/chapter01/theater/Bag.java
+++ src/chapter01/theater/Bag.java
@@ -2,35 +2,29 @@ package chapter01.theater;
 
 public class Bag {
     private Long amount;
-    private Invitation invitation;
     private Ticket ticket;
+    private Invitation invitation;
 
-    public Bag(long amount) {
-        this(null, amount);
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 
-    public Bag(Invitation invitation, long amount) {
-        this.invitation = invitation;
-        this.amount = amount;
-    }
-
-    public boolean hasInvitation() {
-        return invitation != null;
-    }
-
-    public boolean hasTicket() {
-        return ticket != null;
-    }
-
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
-        this.amount -= amount;
+    private boolean hasInvitation() {
+        return invitation != null;
     }
 
-    public void plusAmount(Long amount) {
-        this.amount += amount;
+    private void minusAmount(Long amount) {
+        this.amount -= amount;
     }
 }
