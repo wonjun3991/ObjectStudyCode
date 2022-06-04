b9a2b2cdedccc64f0dabfa84bb3eda34ddf9cc7d
wonjun <wonjun.choi@meshkorea.net>
2022-06-04 18:18:57 +0900

    영화 예매 코드 추가

--- src/chapter01/theater/TicketOffice.java
+++ src/chapter01/theater/TicketOffice.java
@@ -8,7 +8,7 @@ public class TicketOffice {
     private Long amount;
     private List<Ticket> tickets = new ArrayList<>();
 
-    public TicketOffice(Long amount, Ticket ... tickets) {
+    public TicketOffice(Long amount, Ticket... tickets) {
         this.amount = amount;
         this.tickets.addAll(Arrays.asList(tickets));
     }
